fc20269e7ab2ea041faafaee7321f945ffc5179b
Shojaei <F.Shojaei@emofid.com>
2024-07-13 14:15:15 +0330

    First Commit

---
+++ Data_Types.js
@@ -0,0 +1,7 @@
+let isIsland=True;
+let Language;
+
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof Language);
---
+++ Values.js
@@ -0,0 +1,6 @@
+let country="Iran";
+let continent="Asia";
+let population=1223432;
+
+
+console.log(country+continent+population);
