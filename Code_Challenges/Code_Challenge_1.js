7a8f91f79597dfb72bf706a19d5453b7a8fcbcf5
farzaadshojaei <farzaadshojaei@gmail.com>
2024-07-15 11:39:47 +0330

    Adding First Code Challenge

---
+++ Code_Challenges/Code_Challenge_1.js
@@ -0,0 +1,10 @@
+let massMark=92.1
+let massJohn=78.6
+let heighMark=1.77
+let heighJohn=1.55
+let markBMI=massJohn/heighMark ** 2;
+let johnBMI=massJohn/(heighJohn* heighJohn)
+const markHigherBMI=markBMI > johnBMI;
+
+console.log(markBMI,johnBMI,markHigherBMI)
+
---
+++ Operators.js
@@ -0,0 +1,17 @@
+let x=10+5;
+let country="Italy";
+let Continent= "Asia"
+let language="Persian"
+
+x +=10;
+x *=5;
+x++;
+x--;
+--x;
+console.log(x);
+
+let countryPopulation=5400;
+countryPopulation/2;
+console.log(countryPopulation > 6)
+const description1= country + 'Is In' + Continent + ', and its' + countryPopulation + 'Million people speak' + language;
+console.log(description1);
