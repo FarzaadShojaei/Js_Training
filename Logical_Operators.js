438696559c4d57551130c985ed305b6ea1c743f0
Farzad Shojaei <farzaadshojaei@gmail.com>
2024-07-22 15:46:53 +0330

    Update Logical_Operators.js

--- Logical_Operators.js
+++ Logical_Operators.js
@@ -1,1 +1,18 @@
+const hasDriversLicense=true;
+const hasGoodVision=true;
 
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+
+if(hasDriversLicense && hasGoodVision){
+    console.log('Sarah is able to drive')
+} else{
+    console.log('Someone else should drive')
+}
+
+const isTired=true;
+
+console.log(hasDriversLicense && hasGoodVision && isTired)
